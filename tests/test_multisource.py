908a27df97adf0aebead7124de3f8f11573f2ebe
joao-fatela <joao.garrettfatela@unicampania.it>
2024-10-07 23:32:58 +0200

    introduce test to check correct processing of multiple receivers

--- tests/test_multisource.py
+++ tests/test_multisource.py
@@ -7,19 +7,85 @@ import sparapy as sp
 
 create_reference_files = False
 
+
+
+def test_multi_receiver_init():
+    """Test initialization of multiple receivers."""
+    X = 1
+    Y = 1
+    Z = 1
+    patch_size = 1
+    ir_length_s = 1.
+    sampling_rate = 100
+    max_order_k = 10
+    speed_of_sound = 343
+    irs_new = []
+    frequencies = np.array([1000])
+    absorption = 0.
+    walls = sp.testing.shoebox_room_stub(X, Y, Z)
+    method= "universal"
+    algo= "order"
+
+    sc_src = pf.Coordinates(0, 0, 1)
+    sc_rec = pf.Coordinates(0, 0, 1)
+
+    src = np.array([.5,.5, .5])
+    rec0 = np.array([.25,.25,.25])
+    rec1 = np.array([.25,.5,.25])
+    rec2 = np.array([.25,.75,.25])
+    rec3 = np.array([.25,.5,.75])
+
+    rec_traj = np.array([rec0,rec1,rec2, rec3])
+
+    ## initialize radiosity class
+    radi = sp.radiosity_fast.DRadiosityFast.from_polygon(walls, patch_size)
+
+    data_scattering = pf.FrequencyData(
+        np.ones((sc_src.csize,sc_rec.csize,frequencies.size)), frequencies)
+
+    # set directional scattering data
+    radi.set_wall_scattering(
+        np.arange(len(walls)), data_scattering, sc_src, sc_rec)
+
+    # set air absorption
+    radi.set_air_attenuation(
+        pf.FrequencyData(
+            np.zeros_like(data_scattering.frequencies),
+            data_scattering.frequencies))
+
+    # set absorption coefficient
+    radi.set_wall_absorption(
+        np.arange(len(walls)),
+        pf.FrequencyData(
+            np.zeros_like(data_scattering.frequencies)+absorption,
+            data_scattering.frequencies))
+
+    # run simulation
+    radi.bake_geometry(ff_method=method,algorithm=algo)
+
+    radi.init_source_energy(src,ff_method=method,algorithm=algo)
+
+    ir = radi.calculate_energy_exchange_receiver(
+            receiver_pos=rec_traj, speed_of_sound=speed_of_sound,
+            histogram_time_resolution=1/sampling_rate,
+            histogram_length=ir_length_s, ff_method=method, algorithm=algo,
+            max_depth=max_order_k )
+    
+    npt.assert_almost_equal(ir[0], ir[2])
+    npt.assert_almost_equal(ir[1], ir[3])
+
 @pytest.mark.parametrize('src', [
     [[2.,1.,1.], [0, 1, 0], [0, 0, 1]],
     [[.5,0.5,.5], [-1, 1, 0], [0, 0, 1]],
-    [[.5,2,2.5], [1, 0, 0], [0, 0, 1]]
     ])
 @pytest.mark.parametrize('rec', [
     [[1.,1.,.5], [-1, 1, 0], [0, 0, 1]],
     ])
 @pytest.mark.parametrize('ord', [
-    1,5,10,20
+    10
     ])
 @pytest.mark.parametrize('ps', [
-    1.5,3
+    1.5
     ])
 def test_reciprocity_shoebox(src,rec,ord,ps):
     """Test if radiosity results are reciprocal in shoebox room."""
