0edba49164d52cecddd784aeecf24e4a78d8eefe
João Fatela <joao.garrettfatela@unicampania.it>
2024-11-25 11:06:31 +0100

    fix ruff

--- tests/test_mesh_operations.py
+++ tests/test_mesh_operations.py
@@ -1,13 +1,10 @@
-import numpy as np
 import numpy.testing as npt
 import pytest
-import os
-import pyfar as pf
 
-import sparapy as sp
 from sparapy.radiosity_fast import blender_helpers as bh
 
-@pytest.mark.parametrize("path", ["./tests/test_data/cube.blend","./tests/test_data/cube.stl"])
+@pytest.mark.parametrize("path",
+                         ["./tests/test_data/cube.blend","./tests/test_data/cube.stl"])
 def test_mesh_simplification(path):
 
     fine, rough = bh.read_geometry_file(path)
