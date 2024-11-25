b5b4f253b513517fe76ce4048c38da59ae8384e0
João Fatela <joao.garrettfatela@unicampania.it>
2024-11-25 15:17:10 +0100

    introduce visibility check

--- tests/test_visibility.py
+++ tests/test_visibility.py
@@ -37,3 +37,29 @@ def test_point_in_polygon(point, plpt, pln):
 
     assert solution==out
 
+
+@pytest.mark.parametrize("point", [
+    np.array([0.,0.,-1.]),
+    np.array([0.,0.,.5]),
+    np.array([0.,3.,-1.]),
+    ])
+@pytest.mark.parametrize("origin", [np.array([0.,0.,1.])])
+@pytest.mark.parametrize("plpt", [
+    np.array([[1.,1.,0.],[-1.,1.,0.],[-1.,-1.,0.],[1.,-1.,0.]])
+    ])
+@pytest.mark.parametrize("pln", [
+    np.array([0.,0.,1.]),
+    np.array([0.,.5,-.5])/np.linalg.norm(np.array([0.,.5,-.5]))
+    ])
+def test_basic_visibility(point, origin, plpt, pln):
+    """Test basic_visibility function."""
+    out = vh.basic_visibility(patch_center=point,vis_point=origin,
+                              surf_points=plpt,surf_normal=pln)
+
+    if (abs(point[0])/(-point[2]+1) > 1. or
+        abs(point[1])/(-point[2]+1) > 1) or point[2]>0 or pln[2]<0:
+        solution = 1
+    else:
+        solution = 0
+
+    assert solution==out
