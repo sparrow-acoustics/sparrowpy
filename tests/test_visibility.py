994f09ee88dd3851c3d12e0121b7fd4b3d643b7b
João Fatela <joao.garrettfatela@unicampania.it>
2024-12-13 15:11:04 +0100

    cleanup tests

--- tests/test_visibility.py
+++ tests/test_visibility.py
@@ -53,7 +53,7 @@ def test_point_in_polygon(point, plpt, pln):
     ])
 def test_basic_visibility(point, origin, plpt, pln):
     """Test basic_visibility function."""
-    out = vh.basic_visibility(patch_center=point,vis_point=origin,
+    out = vh.basic_visibility(eval_point=point,vis_point=origin,
                               surf_points=plpt,surf_normal=pln)
 
     if (abs(point[0])/(-point[2]+1) > 1. or
@@ -63,48 +63,4 @@ def test_basic_visibility(point, origin, plpt, pln):
         solution = 0
 
     assert solution==out
-
-
-@pytest.mark.parametrize("l1", [
-    np.array([[-1.,0.],[1.,0.]])
-    ])
-@pytest.mark.parametrize("l2", [
-    np.array([[0.,1.],[0.,-1.]])
-    ])
-@pytest.mark.parametrize("solution", [
-    np.array([0.,0.])
-    ])
-def test_line_intersection(l1,l2,solution):
-    """Test basic_visibility function."""
-    out = vh.line_line_int(l1[0],l1[1],l2[0],l2[1])
-
-    npt.assert_array_equal(out,solution)
-
-@pytest.mark.parametrize("surf", [
-    np.array([[0.,2.,0.],[-2.,2.,0.],[-2.,-2.,0.],[0.,-2.,0.]])
-    ])
-@pytest.mark.parametrize("patch", [
-    np.array([[1.,1.,0.],[-1.,1.,0.],[-1.,-1.,0.],[1.,-1.,0.]])
-    ])
-@pytest.mark.parametrize("solution", [
-    [[np.array([[0,3],[2,3]]),
-     np.array([[0.,1.,0.],[0.,-1.,0.]])
-    ],
-    [np.array([[3,0],[3,2]]),
-     np.array([[0.,1.,0.],[0.,-1.,0.]])
-    ]]
-    ])
-def test_intersection_finder(patch,surf,solution):
-    """Test intersection finding function."""
-    for i in range(2):
-        bdbd =vh.find_all_intersections(poly1=patch,poly2=surf)
-
-        conn = bdbd[:,:2].astype(int)
-        pts = bdbd[:,2:]
-
-        npt.assert_array_equal(conn, solution[i][0])
-        npt.assert_array_equal(pts, solution[i][1])
-
-        temp = patch
-        patch = surf
-        surf = temp
+    
