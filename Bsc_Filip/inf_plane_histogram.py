01ae9ce7e089d2087e6310045e7e1d7b98eb8c09
João Fatela <joao.garrettfatela@unicampania.it>
2025-01-10 17:13:11 +0100

    function to generate plane

--- Bsc_Filip/inf_plane_histogram.py
+++ Bsc_Filip/inf_plane_histogram.py
@@ -1,1 +1,39 @@
+"""functions to generate energy histogram of "infinite" plane scenario."""
 import sparapy as sp
+import numpy as np
+
+def infinite_plane(point:np.ndarray, ratio=15.):
+    """Create an "infinite" xy plane relative to eval point position.
+
+    Parameters
+    ----------
+    point: np.array(3,)
+        evaluation position in 3D space (receiver)
+    ratio: float
+        ratio between plane dimensions and
+        distance between receiver and plane.
+
+    Returns
+    -------
+    plane: sparapy.geometry.Polygon
+        plane surface in format compatible with sparapy.
+
+    """
+    length = ratio*point[2] # dimensions of plane's side
+
+    return [
+            sp.geometry.Polygon([ [length/2, length/2, 0],
+                                [-length/2, length/2, 0],
+                                [-length/2, -length/2, 0],
+                                [length/2, -length/2, 0]
+                                ])
+    ]
+
+def get_histogram():
+    """Generate histogram of infinite plane scenario."""
+    source = np.array([0.,0.,1.])
+    receiver = np.array([0.,0.,1.])
+
+    plane = infinite_plane(point=receiver)
+
+
