58357e03661e5b6621e7fb107059d213a3cda294
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-01-12 13:06:23 +0100

    add utils to init

--- sparrowpy/__init__.py
+++ sparrowpy/__init__.py
@@ -14,6 +14,7 @@ from .radiosity_fast.radiosity_class import DRadiosityFast
 from . import testing
 from . import radiosity_fast
 from . import brdf
+from . import utils
 
 
 __all__ = [
@@ -24,4 +25,5 @@ __all__ = [
     'radiosity_fast',
     'testing',
     'brdf',
+    'utils',
 ]
