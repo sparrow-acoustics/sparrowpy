d3f484b7847175f1a7002639572ab6d3f58c034d
joao-fatela <joao.garrettfatela@unicampania.it>
2025-02-07 15:15:22 +0100

    separate checks a bit better

--- tests/test_blender_import.py
+++ tests/test_blender_import.py
@@ -1,12 +1,40 @@
 import numpy.testing as npt
 import pytest
+import numpy as np
 
 import sparrowpy.utils.blender as bh
 
 @pytest.mark.parametrize("path",
                          ["./tests/test_data/cube.blend","./tests/test_data/cube.stl"])
-def test_mesh_simplification(path):
+def test_basic_features(path):
 
     walls,patches = bh.read_geometry_file(path)
 
+    assert len(walls["conn"])==12
+    assert len(walls["normal"])==len(walls["conn"])
+    assert len(walls["verts"])==8
+    assert patches["conn"].shape[1]==3
+    assert patches["verts"].shape[1]==3
+    assert len(walls["normal"][0])==3
+    assert np.max(patches["wall_ID"])==len(walls["conn"])-1
 
+@pytest.mark.parametrize("path",
+                         ["./tests/test_data/cube.blend","./tests/test_data/cube.stl"])
+def test_material_assignment(path):
+    walls,_ = bh.read_geometry_file(path)
+    if path.endswith(".blend"):
+        assert len(walls["material"])==len(walls["conn"])
+        assert isinstance(walls["material"][0], str)
+    else:
+        assert len(walls["material"])==0
+    assert True
+
+@pytest.mark.parametrize("path",
+                         ["./tests/test_data/cube.blend","./tests/test_data/cube.stl"])
+def test_patch_generation(path):
+
+    walls,patches = bh.read_geometry_file(path,max_patch_size=3.)
+
+    ## check if new patches are generated
+    # in case max patch size is larger than max wall side
+    assert patches["conn"].shape[0]==len(walls["conn"])
