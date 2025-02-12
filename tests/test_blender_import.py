5e6aeccd99266a10c0ccf38e40dc4e0a546a899c
joao-fatela <joao.garrettfatela@unicampania.it>
2025-02-12 17:04:02 +0100

    update some tests in agreement with new logic

--- tests/test_blender_import.py
+++ tests/test_blender_import.py
@@ -6,25 +6,40 @@ import sparrowpy.utils.blender as bh
 import os
 
 @pytest.mark.parametrize("path",
-                         ["./tests/test_data/cube.blend","./tests/test_data/cube.stl"])
+                         ["./tests/test_data/cube_simple.blend","./tests/test_data/cube.stl"])
 
-def test_basic_features(path):
+def test_geometry_loading(path):
 
-    walls,patches = bh.read_geometry_file(path)
+    walls = bh.read_geometry_file(path, auto_walls=True)
 
-    assert len(walls["conn"])==12
-    assert len(walls["normal"])==len(walls["conn"])
-    assert len(walls["verts"])==8
-    assert patches["conn"].shape[1]==3
-    assert patches["verts"].shape[1]==3
-    assert len(walls["normal"][0])==3
-    assert np.max(patches["wall_ID"])==len(walls["conn"])-1
+    assert walls["conn"].shape[0]==6
+    assert walls["normal"].shape[0]==walls["conn"].shape[0]
+    assert walls["verts"].shape[0]==8
+    assert walls["normal"].shape[1]==3
+    
+    
+def test_auto_wall():
+    """Test if auto/manual wall assignment is working as expected"""
+    # check if sub-walls of consistent material are merged or individually picked up
+    simple_merge = bh.read_geometry_file("./tests/test_data/cube_simple.blend", auto_walls=True)
+    simple_split = bh.read_geometry_file("./tests/test_data/cube_simple.blend", auto_walls=False)
+    
+    assert simple_merge["conn"].shape[0]!=simple_split["conn"].shape[0]
+    assert simple_split["conn"].shape[0]==24
+    
+    # check if sub-walls of different material are merged or individually picked up
+    dual_merge = bh.read_geometry_file("./tests/test_data/cube_subdiv.blend", auto_walls=True)
+    dual_split = bh.read_geometry_file("./tests/test_data/cube_subdiv.blend", auto_walls=False)
+    
+    assert dual_merge["conn"].shape[0]==10
+    npt.assert_array_equal(dual_merge["conn"],dual_split["conn"])
+
 
 @pytest.mark.parametrize("path",
-                         ["./tests/test_data/cube.blend","./tests/test_data/cube.stl"])
+                         ["./tests/test_data/cube_simple.blend","./tests/test_data/cube.stl"])
 def test_material_assignment(path):
     """Check that material is correctly assigned from model."""
-    walls,_ = bh.read_geometry_file(path)
+    walls = bh.read_geometry_file(path)
     if path.endswith(".blend"):
         assert len(walls["material"])==len(walls["conn"])
         assert isinstance(walls["material"][0], str)
