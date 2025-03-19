3a627becd7ac4a1b0d2e306c5b84a133a7293ffc
joao-fatela <joao.garrettfatela@unicampania.it>
2025-03-19 10:17:15 +0100

    remove pyplot from tests

--- tests/test_visibility.py
+++ tests/test_visibility.py
@@ -5,7 +5,6 @@ import numpy as np
 import sparrowpy.radiosity_fast.geometry as geom
 bpy = pytest.importorskip("bpy")
 import sparrowpy.utils.blender as bh  # noqa: E402
-import matplotlib.pyplot as plt  # noqa: E402
 
 @pytest.mark.parametrize("origin", [np.array([0.,1.,3.])])
 @pytest.mark.parametrize("point", [np.array([0.,1.,-1])])
