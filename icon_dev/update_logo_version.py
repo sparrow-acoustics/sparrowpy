ef50937244108aa8504c29f4d4557269dc6c5597
joao-fatela <joao.garrettfatela@unicampania.it>
2025-04-13 21:57:31 +0200

    simplify function, no need for command line input

--- icon_dev/update_logo_version.py
+++ icon_dev/update_logo_version.py
@@ -1,50 +1,45 @@
 """Updates icons and logos to most recent version."""
-import sys
 import os
 from PIL import Image
+import sparrowpy
 
-version=sys.argv[1]
+def main():
+    version=sparrowpy.__version__
 
-out_path = "docs/_static"
+    out_path = "docs/_static"
 
-svg_filename = "logo.svg"
-png_filename = "logo.png"
-ico_filename = "favicon.ico"
+    svg_filename = "logo.svg"
+    ico_filename = "favicon.ico"
 
-out_filepath = os.path.join(os.getcwd(),
-                      out_path,
-                      svg_filename)
+    original_svg=open(os.path.join(os.getcwd(),
+                                'icon_dev',
+                                'logo.svg'),
+                    'r')
 
-original_svg=open(os.path.join(os.getcwd(),
-                               'icon_dev',
-                               'logo.svg'),
-                  'r')
+    out=open(os.path.join(os.getcwd(),
+                        out_path,
+                        svg_filename),
+            'w')
 
-out=open(os.path.join(os.getcwd(),
-                      out_path,
-                      svg_filename),
-         'w')
+    # read template svg as string
+    svg_string = original_svg.read()
 
+    # replace standin version number
+    svg_string = svg_string.replace('>[stand-in]<',
+                                    f'>{version}<')
 
+    # write logo svg
+    out.write(svg_string)
 
-svg_string = original_svg.read()
+    img = Image.open(os.path.join(os.getcwd(),
+                        "icon_dev",
+                        "logo_no_version.png",
+                        ))
 
-#replace standin with actual version
-svg_string = svg_string.replace('>[stand-in]<',
-                                f'>{version}<')
+    img.save(os.path.join(os.getcwd(),
+                        out_path,
+                        ico_filename,
+                        ))
 
-
-
-out.write(svg_string)
-
-img = Image.open(os.path.join(os.getcwd(),
-                      "icon_dev",
-                      "logo_no_version.png",
-                      ))
-
-img.save(os.path.join(os.getcwd(),
-                      out_path,
-                      ico_filename,
-                      ))
-
-
+if __name__=="__main__":
+    main()
