0bdcce38e0638b2f954f3071d6a505dad12700f2
joao-fatela <joao.garrettfatela@unicampania.it>
2025-04-14 17:13:24 +0200

    fix one of the tests

--- tests/test_multisource.py
+++ tests/test_multisource.py
@@ -175,6 +175,7 @@ def test_reciprocity_s2p_p2r(src,rec,method="universal"):
                                                     source_position=src_.position,
                                                     patches_center=np.array([wall[0].center]),
                                                     patches_points=np.array([wall[0].pts]),
+                                                    patches_normals=np.array([[1.,0.,0.]]),
                                                     air_attenuation=air_att,
                                                     n_bins=1,
                                                     )
