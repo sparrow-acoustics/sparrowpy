7b9c46ced39d79003c6bf53ddfd56f8a4e6fb02a
joao-fatela <joao.garrettfatela@unicampania.it>
2025-04-15 14:23:00 +0200

    fix point2patch visibility check

--- sparrowpy/classes/RadiosityFast.py
+++ sparrowpy/classes/RadiosityFast.py
@@ -439,12 +439,11 @@ class DirectionalRadiosityFast():
         brdf_index = self._brdf_index
         patches_center = self.patches_center
         patches_normal = self.patches_normal
-        source_vis = np.array(
-            [geometry._basic_visibility(vis_point=source_position,
-                                  eval_point=patches_center[i],
-                                  surf_points=self.patches_points[i],
-                                  surf_normal=patches_normal[i])
-                                  for i in range(patches_center.shape[0])])
+        source_vis = geometry._check_point2patch_visibility(
+                                        vis_point=source_position,
+                                        eval_point=patches_center,
+                                        surf_points=self.patches_points,
+                                        surf_normal=patches_normal)
 
         energy_0, distance_0 = form_factor._source2patch_energy_universal(
             source_position, patches_center, self.patches_points,
