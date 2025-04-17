fa6ec767442bdb219653cc87317987908cedf91b
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2025-04-17 15:59:12 +0200

    Apply suggestions from code review

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'autodocsumm',
     'sphinx_design',
+    'sphinx_favicon',
     'sphinx_mdinclude',
     'nbsphinx',
     'nbsphinx_link',
@@ -110,6 +111,7 @@ html_static_path = ['_static']
 html_css_files = ['css/custom.css']
 html_logo = '_static/logo.png'
 html_title = "sparrowpy"
+html_favicon = '_static/favicon.ico'
 
 # -- HTML theme options
 # https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/layout.html
