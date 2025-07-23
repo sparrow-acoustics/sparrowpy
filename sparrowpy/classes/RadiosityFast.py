0742e23e8d2951f37ec0956d7c6b6cf5718f3424
joao-fatela <joao.garrettfatela@unicampania.it>
2025-07-23 17:20:52 +0200

    back to more elegant get_scattering_data_receiver_index

--- sparrowpy/classes/RadiosityFast.py
+++ sparrowpy/classes/RadiosityFast.py
@@ -1296,16 +1296,12 @@ def get_scattering_data_receiver_index(
 
     for i in range(n_patches):
         difference_receiver = pos_j-pos_i[i]
-        norm = np.linalg.norm(
-            difference_receiver)
 
-        if norm > 1e-6:
-            difference_receiver /= norm
-            receiver_idx[i] = np.argmin(np.sum(
-                (receivers[wall_id_i[i], :]-difference_receiver)**2, axis=-1),
-                axis=-1)
-        else:
-            receiver_idx[i]=n_patches
+        difference_receiver /= np.linalg.norm(
+            difference_receiver)
+        receiver_idx[i] = np.argmin(np.sum(
+            (receivers[wall_id_i[i], :]-difference_receiver)**2, axis=-1),
+            axis=-1)
 
     return receiver_idx
 
