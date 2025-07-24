d91c94140702469ffa1e05d0c5cc47bdf90aeda8
joao-fatela <joao.garrettfatela@unicampania.it>
2025-07-24 10:39:20 +0200

    update sampling with proper weights on run_simu

--- examples/run_simu.py
+++ examples/run_simu.py
@@ -10,17 +10,16 @@ import time
 def run_simu_mem(walls, source, receiver,
              patch_size=1, absorption=.1, scattering=1,
              speed_of_sound=343.26, time_step=.1, duration=.5,
-             refl_order=3, freq=np.array([1000]), res=30):
+             refl_order=3, freq=np.array([1000]), res=4):
 
     att=pyrato.air_attenuation_coefficient(freq)
     att_np= att* 0.115129254650564
 
     # set brdfs
-    samples = pf.samplings.sph_equal_angle(delta_angles=res)
-    samples.weights=np.ones(samples.cshape[0])
-
-    brdf_sources = samples[np.where((samples.elevation*180/np.pi >= 0))].copy()
-    brdf_receivers = samples[np.where((samples.elevation*180/np.pi >= 0))].copy()
+    samples = pf.samplings.sph_gaussian(res)
+    pos_hemisphere = np.where((samples.elevation*180/np.pi > 0))
+    brdf_sources = samples[pos_hemisphere].copy()
+    brdf_receivers = samples[pos_hemisphere].copy()
 
     # create object
     tracemalloc.start()
@@ -72,18 +71,73 @@ def run_simu_mem(walls, source, receiver,
 def run_simu(walls, source, receiver,
              patch_size=1, absorption=.1, scattering=1,
              speed_of_sound=343.26, time_step=.1, duration=.5,
-             refl_order=3, freq=np.array([1000]), res=30):
+             refl_order=3, freq=np.array([1000]), res=4):
 
     t = []
     att=10*np.log10(pyrato.air_attenuation_coefficient(freq))/1000
     att_np= att* 0.115129254650564
 
     # set brdfs
-    samples = pf.samplings.sph_equal_angle(delta_angles=res)
-    samples.weights=np.ones(samples.cshape[0])
+    samples = pf.samplings.sph_gaussian(res)
+    pos_hemisphere = np.where((samples.elevation*180/np.pi > 0))
+    brdf_sources = samples[pos_hemisphere].copy()
+    brdf_receivers = samples[pos_hemisphere].copy()
 
-    brdf_sources = samples[np.where((samples.elevation*180/np.pi >= 0))].copy()
-    brdf_receivers=samples[np.where((samples.elevation*180/np.pi >= 0))].copy()
+    t0=time.time()
+    # create object
+    radi = sp.DirectionalRadiosityFast.from_polygon(walls, patch_size)
+
+    # create directional scattering data (totally diffuse)
+    brdf = sp.brdf.create_from_scattering(
+        brdf_sources,
+        brdf_receivers,
+        pf.FrequencyData(scattering, freq),
+        pf.FrequencyData(absorption, freq))
+
+    # set directional scattering data
+    radi.set_wall_brdf(
+        np.arange(len(walls)), brdf, brdf_sources, brdf_receivers)
+    # set air absorption
+    radi.set_air_attenuation(
+        pf.FrequencyData(
+            att_np,
+            brdf.frequencies))
+    t.append(time.time()-t0)
+
+    t0=time.time()
+    # calculate from factors including brdfs
+    radi.bake_geometry()
+    t.append(time.time()-t0)
+
+    t0=time.time()
+    radi.init_source_energy(source)
+    radi.calculate_energy_exchange(
+        speed_of_sound=speed_of_sound,
+        etc_time_resolution=time_step,
+        etc_duration=duration,
+        max_reflection_order=refl_order)
+    t.append(time.time()-t0)
+
+    t0=time.time()
+    etc_radiosity = radi.collect_energy_receiver_mono(receivers=receiver)
+    t.append(time.time()-t0)
+
+    return etc_radiosity,t
+
+def run_simu_pure(walls, source, receiver,
+             patch_size=1, absorption=.1, scattering=1,
+             speed_of_sound=343.26, time_step=.1, duration=.5,
+             refl_order=3, freq=np.array([1000]), res=4):
+
+    t = []
+    att=10*np.log10(pyrato.air_attenuation_coefficient(freq))/1000
+    att_np= att* 0.115129254650564
+
+    # set brdfs
+    samples = pf.samplings.sph_gaussian(res)
+    pos_hemisphere = np.where((samples.elevation*180/np.pi > 0))
+    brdf_sources = samples[pos_hemisphere].copy()
+    brdf_receivers = samples[pos_hemisphere].copy()
 
     t0=time.time()
     # create object
