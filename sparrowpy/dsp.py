1adf90cf3c9a5cec260dfc1fe78b3beda8b48d0d
joao-fatela <joao.garrettfatela@unicampania.it>
2025-09-09 10:13:39 +0200

    introduce dirac sequence generation function

--- sparrowpy/dsp.py
+++ sparrowpy/dsp.py
@@ -112,3 +112,116 @@ def reflection_density_room(
 
     # return the reflection density and the starting time
     return mu, t_start
+
+def dirac_sequence(
+        reflection_density, n_samples, t_start=0, sampling_rate=44100,
+        seed=None):
+    r"""Dirac sequence based on the reflection density over time.
+
+    The Dirac sequence is generated based on the chapter 5.3.4 of [#]_.
+
+    The time difference between each dirac in the sequence is Poisson
+    distributed and can be calculated based on:
+
+    .. math:: \Delta t_a = \frac{1}{\mu} \cdot \ln{\frac{1}{z}}
+
+    with z being a random number in the range of :math:`z \in (0, 1]`
+    and :math:`\mu` being the ``reflection_density`` in a room.
+    Each dirac has an amplitude of 1 or -1, which is chosen
+    randomly with equal probability.
+    The dirac sequence generation starts after :math:`t_\text{start}`.
+
+    Parameters
+    ----------
+    reflection_density : pyfar.TimeData
+        reflection density :math:`\mu` in :math:`1/s^2` over time.
+        An error is raised if the reflection sensitivity is greater than
+        sampling_rate/2. Schröder suggested a maximum reflection density of
+        sampling_rate/4 :math:`1/s^2`.
+    n_samples : int
+        The length of the dirac sequence in samples.
+    t_start : float
+        The dirac sequence generation starts after :math:`t_\text{start}`
+        in seconds. The default is ``0``.
+    sampling_rate : int, optional
+        The sampling rate of the dirac sequence in Hz.
+        The default is 44100.
+    seed : int, None, optional
+        The seed for the random generator. Pass a seed to obtain identical
+        results for multiple calls. The default is ``None``, which will yield
+        different results with every call.
+        See :py:func:`numpy.random.default_rng` for more information.
+
+    Returns
+    -------
+    dirac_sequence : pyfar.Signal
+        Signal of the generated dirac impulse sequence.
+
+    References
+    ----------
+    .. [#] D. Schröder, “Physically based real-time auralization of
+           interactive virtual environments,” PhD Thesis, Logos-Verlag,
+           Berlin, 2011. [Online].
+           Available: https://publications.rwth-aachen.de/record/50580
+
+    Examples
+    --------
+    Generate a Dirac sequence based on the reflection density of a room
+    with a volume of 5000 m³.
+
+    .. plot::
+
+        >>> import pyfar as pf
+        >>> n_samples = 22050
+        >>> reflection_density, t_0 = pf.signals.reflection_density_room(
+        ...     5000, n_samples)
+        >>> dirac_sequence = pf.signals.dirac_sequence(
+        ...     reflection_density, n_samples, t_start=t_0)
+        >>> ax = pf.plot.time(dirac_sequence, linewidth=.5)
+        >>> ax.set_title("Dirac sequence")
+
+    Generate a Dirac sequence based on a custom reflection density.
+
+    .. plot::
+
+        >>> import pyfar as pf
+        >>> import numpy as np
+        >>> n_samples = 22050
+        >>> reflection_density = pf.TimeData(
+        ...     np.ones(n_samples)*10000, np.arange(n_samples)/44100)
+        >>> dirac_sequence = pf.signals.dirac_sequence(
+        ...     reflection_density, n_samples, t_start=0)
+        >>> ax = pf.plot.time(dirac_sequence, linewidth=.5)
+        >>> ax.set_title("Dirac sequence")
+    """
+    # check input
+    if not isinstance(reflection_density, pf.TimeData):
+        raise ValueError(
+            "reflection_density must be a pf.TimeData object.")
+    if t_start < 0:
+        raise ValueError("t_start must be positive.")
+    if np.any(reflection_density.time > sampling_rate / 2):
+        raise ValueError(
+            "The reflection density must be less than sampling_rate/2.")
+
+    rng = np.random.default_rng(seed)
+    dirac_sequence = pf.Signal(np.zeros(n_samples), sampling_rate)
+    mu_times = reflection_density.times
+    t_current = t_start
+    i_current = np.argmin(np.abs(t_current-mu_times))
+    t_max = reflection_density.times[-1]
+    while True:
+        # calculate next event time
+        z = -rng.uniform(-1, 0) # uniform distribution in (0, 1]
+        # Equation (5.43) interval size
+        delta_ta = 1 / reflection_density.time[..., i_current] * np.log(1 / z)
+        t_current += delta_ta
+
+        if t_current > t_max:
+            break
+
+        i_current = np.argmin(np.abs(t_current-mu_times))
+
+        dirac_sequence.time[..., i_current] = rng.choice([-1, 1], p=[0.5, 0.5])
+
+    return dirac_sequence
