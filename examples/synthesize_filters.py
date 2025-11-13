0cdb62ddf66ac920618995a7f9c1d443c9ee6ca2
joao-fatela <joao.garrettfatela@unicampania.it>
2025-11-13 14:59:47 +0100

    fix surface filtering handling

--- examples/synthesize_filters.py
+++ examples/synthesize_filters.py
@@ -13,10 +13,10 @@ def run(test=True,
                               "phd","listening experiment",
                               "synthesis","lib") ):
     # %% settings
-    sampling_rate = 44100 # Hz
+    sampling_rate = 48000 # Hz
 
     # %% load data
-    print("\n\033[93m loading etc data...\033[00m", end=" ")
+    print("\n\033[93m loading radi data...\033[00m", end=" ")
 
     if test:
         geom_id = "radi_test"
@@ -28,25 +28,24 @@ def run(test=True,
     etcfiles = glob(os.path.join(base_dir,
                                  "etcs",
                                  "*"+geom_id+"_patchwise_pos"+"*.far"))
-    etc_data = pf.io.read(etcfiles[0])
-    etc=etc_data["etc"]
-    patch_filter = etc_data["patch_filter"]
-    duration = etc.times[-1]
+    duration = 6
     print("\033[92m Done!\033[00m")
 
 
     print("\n\033[93m generating and filtering noise signal...\033[00m", end=" ")
-    refl_density = pf.TimeData(data = sampling_rate/2*np.ones_like(etc.times),
-                               times=etc.times)
+    # refl_density = pf.TimeData(data = sampling_rate/2*np.ones_like(etc.times),
+    #                            times=etc.times)
 
     noisefilename = os.path.join(base_dir,"filters","noise.far")
 
     if not os.path.exists(noisefilename):
 
-        noise = sp.dsp.dirac_sequence(n_samples=int(duration*sampling_rate),
-                                    reflection_density=refl_density,
-                                    sampling_rate=sampling_rate)
+        # noise = sp.dsp.dirac_sequence(n_samples=int(duration*sampling_rate),
+        #                             reflection_density=refl_density,
+        #                             sampling_rate=sampling_rate)
 
+        noise = pf.signals.noise(n_samples = sampling_rate*duration,
+                                 sampling_rate=sampling_rate)
         noise_filtered,bw = sp.dsp.band_filter_signal(signal=noise,
                                                 frequencies=np.array(pf.dsp.filter.fractional_octave_frequencies()[0]),
                                                 num_fractions=1)
@@ -65,19 +64,22 @@ def run(test=True,
     print("\033[92m Done!\033[00m")
 
 
-    print("\n\033[93m loading and filtering hrirs...\033[00m", end=" ")
+    print("\n\033[93m loadinghrirs...\033[00m", end=" ")
     hrir,hrir_coords,_=pf.io.read_sofa(os.path.join(base_dir,"FABIAN_HRIR_measured_HATO_0.sofa"))
-    hrir_ids = hrirs_per_patch(coords=hrir_coords,
-                               patch_positions=radi.patches_center[patch_filter,:])
-
-    hrir = hrir[hrir_ids,:]
     print("\033[92m Done!\033[00m")
 
     print("\n\033[93m convolving position-wise filters with hrirs...\033[00m",
           end="\n")
     for i, etc_path in enumerate(tqdm(etcfiles)):
 
-        etc = pf.io.read(etc_path)["etc"]
+        etc_data = pf.io.read(etc_path)
+        etc = etc_data["etc"]
+        patch_filter = etc_data["patch_filter"]
+
+        hrir_ids = hrirs_per_patch(coords=hrir_coords,
+                                   patch_positions=radi.patches_center[patch_filter])
+
+        hrir_selection = hrir[hrir_ids,:]
 
         filter_patchwise = sp.dsp.weight_signal_by_etc(energy_time_curve=etc,
                                                    signal=noise_filtered,
@@ -85,12 +87,17 @@ def run(test=True,
         filter_patchwise.time = np.repeat(
             np.sum(filter_patchwise.time, axis=1)[:,np.newaxis,:],2,axis=1)
 
-        out_filter = pf.dsp.convolve(signal1=filter_patchwise, signal2=hrir)
+        out_filter = pf.dsp.convolve(signal1=filter_patchwise,
+                                     signal2=hrir_selection)
 
         out_filter.time = np.sum(out_filter.time, axis=0)
 
         pf.io.write(os.path.join(base_dir,"filters",geom_id+"_filter_"+str(i)+".far"),
                     bin_filter=out_filter,compress=False)
+
+        del out_filter
+        del filter_patchwise
+        del etc
     print("\033[92m Done!\033[00m")
 
 
@@ -119,7 +126,7 @@ if __name__ == "__main__":
 
     args = sys.argv[1:]
 
-    test = True
+    test = False
     base_dir=os.path.join(os.getcwd(),"..","..",
                           "phd","listening experiment",
                           "synthesis","lib")
